bab0f3750934e57153a8fe18e784bd0eb9cfe6a4
nepthius <anishdshekar@gmail.com>
2024-11-16 21:15:11 -0500

    add initial rds ci/cd pipeline

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,9 @@
 FROM --platform=linux/amd64 python:3.12-slim
 
+#postgres download and setup
+RUN apt-get update
+RUN apt-get install -y postgresql libpq-dev postgresql-client postgresql-client-common gcc
+
 #add user to prevent sudo commands running
 RUN useradd -r -s /bin/bash anish
 
@@ -17,10 +21,18 @@ ENV FLASK_ENV=production
 ARG AWS_ACCESS_KEY_ID
 ARG AWS_SECRET_ACCESS_KEY
 ARG AWS_DEFAULT_REGION
+ARG POSTGRES_USER
+ARG POSTGRES_PW
+ARG POSTGRES_HOST
+ARG POSTGRES_DB
 
 ENV AWS_ACCESS_KEY_ID $AWS_ACCESS_KEY_ID
 ENV AWS_SECRET_ACCESS_KEY $AWS_SECRET_ACCESS_KEY
 ENV AWS_DEFAULT_REGION $AWS_DEFAULT_REGION
+ENV POSTGRES_USER $POSTGRES_USER
+ENV POSTGRES_PW $POSTGRES_PW
+ENV POSTGRES_HOST $POSTGRES_HOST
+ENV POSTGRES_DB $POSTGRES_DB
 
 ADD ./requirements.txt ./requirements.txt
 
--- app.py
+++ app.py
@@ -1,7 +1,58 @@
-from flask import Flask
+import os
+
+from dotenv import load_dotenv
+from flask import Flask, request, jsonify
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+load_dotenv()
+
+app.config['SQLALCHEMY_DATABASE_URI'] = (
+    f'postgresql+psycopg2://{os.getenv("POSTGRES_USER")}:' +
+    f'{os.getenv("POSTGRES_PW")}@' +
+    f'{os.getenv("POSTGRES_HOST")}/' +
+    f'{os.getenv("POSTGRES_DB")}'
+)
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+class Vehicle(db.Model):
+    __tablename__='vehicles'
+
+    vin = db.Column(db.String(), primary_key=True)
+    manafacturer_name = db.Column(db.String())
+    horse_power = db.Column(db.Integer)
+    model_name = db.Column(db.String())
+    model_year = db.Column(db.Integer)
+    purchase_price = db.Column(db.Float)
+    fuel_type = db.Column(db.String())
+
+    def __init__(self, vin, manafacturer_name, horse_power, model_name, model_year, purchase_price, fuel_type):
+        self.vin = vin.upper()
+        self.manafacturer_name = manafacturer_name
+        self.horse_power = horse_power
+        self.model_name = model_name
+        self.model_year = model_year
+        self.purchase_price = purchase_price
+        self.fuel_type = fuel_type
+    
+    def __repr__(self):
+        return f'vin: {self.vin}'
+
 
 @app.route('/')
 def hello() -> str:
     return 'Start of vehicle creation - round 3!'
+
+@app.route('/vehicle')
+def list_vehicles():
+    vehicles = Vehicle.query.all()
+    if vehicles:
+        return 'There are vehicles'
+    else:
+        return 'No Vehicles.'
